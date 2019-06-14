3d68c6709123fe0bcf67ee3320485f68745b85c1
mota <mepuedeayudar@hotmail.com>
2019-06-14 12:17:54 -0400

    add random

---
+++ src/random.c
@@ -0,0 +1,13 @@
+#include "random.h"
+
+void
+setSeed(int64_t s)
+{
+    seed = (s ^ 0x5DEECE66DL) & ((1LL << 48) - 1);
+}
+
+uint8_t
+nextChar(void){
+    seed = (seed * 0x5DEECE66DL + 0xBL) & ((1LL << 48) - 1);
+    return (uint8_t)(seed>>40);
+}
